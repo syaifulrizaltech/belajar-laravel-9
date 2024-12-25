a169df830ff27a7527a6e99871c74909faa51923
xGojo10 <m.syaifulrizal96@gmail.com>
2024-12-25 08:44:06 +0800

    belajar router/routes/routing

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+<body>
+    <h1>Ini Adalah Contact</h1>
+    <h2>{{$name}}</h2>
+    <h2>{{$phone}}</h2>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function(){
+    return 9*9;
+});
+
+Route::get('/contact', function(){
+    return view('contact', ['name' => 'M. Saiful Rizal', 'phone' => '08123456789']);
+});
+
+// cara simpel
+// Route::view('/contact', 'contact', ['name' => 'M. Saiful Rizal', 'phone' => '08123456789']);
+
+// Redirect mendefinisikan rute yang mengalihkan ke URI lain
+Route::redirect('/contact', '/contact-us');
+// end
+
+
+// Parameter mengambil segmen URI dalam rute Anda. Misalnya, Anda mungkin perlu mengambil ID pengguna dari URL. 
+Route::get('/product', function(){
+    return 'product';
+});
+
+Route::get('/product/{id}', function($id){
+    // return 'Ini Adalah Product Dengan ID ' .$id;
+    return view('product.detail', ['id' => $id]);
+});
+// end
+
+
+// Prefix dapat digunakan untuk memberi awalan pada setiap rute dalam grup dengan URI tertentu.
+Route::prefix('admin')->group(function () {
+    Route::get('/users', function () {
+        // Matches The "/admin/users" URL
+    });
+    Route::get('/profil-admin', function(){
+        return 'Profil Admin';
+    });
+    
+    Route::get('/about-admin', function(){
+        return 'About Admin';
+    });
+    
+    Route::get('/contact-admin', function(){
+        return 'Contact Admin';
+    });
+});
+// end
